2bf53b88f068b9786e2836351ce24371a2a3b52f
4545bk <natnaelgizawayano@gmail.com>
2025-03-07 01:57:36 +0300

    add map2

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,14 +3,18 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Home from './pages/Home';
 import Dashboard from './pages/Dashboard';
 import SafetyGuides from './pages/SafetyGuides';
-
+import previous from './pages/previous';
 function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Dashboard />} />
         <Route path="/home" element={<Home />} />
+        
         <Route path="/safety-guides" element={<SafetyGuides />} />
+
+        <Route path="/previous" element={<previous/>} />
+
       </Routes>
     </BrowserRouter>
   );
