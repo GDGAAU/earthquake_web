35e116dd8082d95e6c24fd0b941a63dd15b46b26
Jubalme <bethelasnake0000@gmail.com>
2025-03-07 00:24:43 +0300

    dashboard

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,13 +1,15 @@
 import React from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Home from './pages/Home';
+import Dashboard from './pages/Dashboard';
 import SafetyGuides from './pages/SafetyGuides';
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Home />} />
+        <Route path="/" element={<Dashboard />} />
+        <Route path="/home" element={<Home />} />
         <Route path="/safety-guides" element={<SafetyGuides />} />
       </Routes>
     </BrowserRouter>
